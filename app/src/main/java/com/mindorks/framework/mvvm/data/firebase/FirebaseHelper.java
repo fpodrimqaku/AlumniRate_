e0dc045e0124b01e9de1a462be6e24d34e946899
FATON.P <faton.p@gjirafa.com>
2022-05-26 13:53:08 +0200

    Updated gradle version from a stone era version to 5.x

--- app/src/main/java/com/mindorks/framework/mvvm/data/firebase/FirebaseHelper.java
+++ app/src/main/java/com/mindorks/framework/mvvm/data/firebase/FirebaseHelper.java
@@ -1,7 +1,6 @@
 package com.mindorks.framework.mvvm.data.firebase;
 
 import androidx.annotation.NonNull;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
